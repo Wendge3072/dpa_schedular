f4685758c680109ed87a2cae73cf286e9bbad1af
Wendge3072 <wendge@outlook.com>
2026-05-27 17:13:37 +0800

    exp

--- packet_processor/dev/flexio_pp_dev_utils.h
+++ packet_processor/dev/flexio_pp_dev_utils.h
@@ -58,7 +58,7 @@ sch 线程 rollover 模式切换开关
 
 #define assert_debug 0
 
-#define DEFAULT_LINK_BANDWIDTH_BPS (200 * 1000000000ULL)
+#define DEFAULT_LINK_BANDWIDTH_BPS (45 * 1000000000ULL)
 #define MAX_CYCLE_PERCENTAGE 7800
 #define MAX_CYCLE_TOTAL 10000
 #define WORKER_BATCH_SIZE 1048576UL
@@ -80,7 +80,7 @@ sch 线程 rollover 模式切换开关
 static uint32_t cycle_weights[MAX_TENANT_NUM] = {1, 2};
 
 // static uint32_t bandwidth_weights[MAX_TENANT_NUM] = {40, 60};
-static uint32_t bandwidth_weights[MAX_TENANT_NUM] = {1, 1};
+static uint32_t bandwidth_weights[MAX_TENANT_NUM] = {1, 2};
 // static uint32_t bandwidth_weights[MAX_TENANT_NUM] = {30, 60};
 
 enum pp_workload_type {
